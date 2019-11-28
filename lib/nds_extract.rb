7338b6f50f03345ae16a9b3ecc26a6217dbcba35
Luke Lin <lukelin1991@yahoo.com>
2019-11-28 01:51:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,6 +37,7 @@ end
 def total_gross(source)
   index = 0
   total_of_total = 0
+  director = 0
   #while index < list_of_directors(source).length do
   return directors_totals(list_of_directors(source[index]))
 
